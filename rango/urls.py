3b7485712fa2d7e1eca8fc38256b43d9eab46f5c
Eric Ma <mcr.988899@163.com>
2022-01-27 23:24:16 +0000

    chapter 3.5 finished

---
+++ rango/urls.py
@@ -0,0 +1,13 @@
+# -*- coding = utf-8 -*-
+# @TIME : 2022/1/27 23:20
+# @Author : Eric Ma
+# @File : urls.py
+# @Software : PyCharm
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
